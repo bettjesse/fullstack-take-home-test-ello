1c3bdb0753bf9bf9e7fb3bd7bb239d7d0c3b655e
jesse <jessebett1999@gmail.com>
2024-06-14 12:51:57 +0300

    updates with pagination and error message

--- frontend/ello-frontend/src/components/Book.tsx
+++ frontend/ello-frontend/src/components/Book.tsx
@@ -1,17 +1,15 @@
-
 import { useState, useRef, useEffect } from 'react';
 import SkeletonCard from './SkeletonCard';
 import BookCard from './BookCard';
-import { TextField, Box, Typography, Grid, Container, IconButton, Skeleton, Badge, AppBar, Toolbar,  Snackbar } from '@mui/material';
+import TeacherFavorites from './TeacherFavorites';
+import { TextField, Box, Typography, Grid, Container, IconButton, Skeleton, Badge, AppBar, Toolbar, Snackbar, Button } from '@mui/material';
 import ReadingListDrawer from './ReadingListDrawer';
-import { AddCircleOutline, MenuBook, CheckCircleOutline,  } from '@mui/icons-material';
+import { AddCircleOutline, MenuBook, CheckCircleOutline } from '@mui/icons-material';
 import { useAppDispatch, useAppSelector } from '../hooks/useAppDispatch';
 import { addBook, removeBook } from '../slices/readingList';
 import { CssBaseline, GlobalStyles } from '@mui/material';
 import { gql, useQuery } from '@apollo/client';
 import { RootState } from '../store';
-
-
 
 // Define the Books query
 const BOOKS_QUERY = gql`
@@ -41,14 +39,18 @@ const getRandomBooks = (books: Book[], num: number): Book[] => {
 const Books = () => {
   const { loading, error, data } = useQuery<{ books: Book[] }>(BOOKS_QUERY);
   const [searchTerm, setSearchTerm] = useState('');
+  const [hasError, setHasError] = useState(false); 
   const [filteredBooks, setFilteredBooks] = useState<Book[]>([]);
-  const [randomBooks, setRandomBooks] = useState<Book[]>([]);
+  const [teacherFavorites, setTeacherFavorites] = useState<Book[]>([]);
+  const [allBooks, setAllBooks] = useState<Book[]>([]);
+  const [page, setPage] = useState(1);
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const dispatch = useAppDispatch();
   const readingListCount = useAppSelector((state: RootState) => state.readingList.books.length);
   const readingList = useAppSelector(state => state.readingList.books);
   const searchRef = useRef<HTMLDivElement>(null);
   const [drawerOpen, setDrawerOpen] = useState(false);
+  const booksPerPage = 8; // Total books to show
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
@@ -65,7 +67,9 @@ const Books = () => {
 
   useEffect(() => {
     if (data) {
-      setRandomBooks(getRandomBooks(data.books, 9));
+      const shuffledBooks = getRandomBooks(data.books, booksPerPage); // Get 8 random books
+      setTeacherFavorites(shuffledBooks.slice(0, 8)); // Show 4 on desktop
+      setAllBooks(shuffledBooks); // Show all books initially
     }
   }, [data]);
 
@@ -74,6 +78,7 @@ const Books = () => {
     if (data) {
       const filtered = data.books.filter(book => book.title.toLowerCase().includes(term.toLowerCase()));
       setFilteredBooks(filtered);
+      setHasError(filtered.length === 0); 
     }
   };
 
@@ -92,6 +97,15 @@ const Books = () => {
 
   const isBookInReadingList = (book: Book) => {
     return readingList.some(item => item.title === book.title);
+  };
+
+  const loadMoreBooks = () => {
+    if (data) {
+      const nextPage = page + 1;
+      const newBooks = data.books.slice(0, nextPage * booksPerPage);
+      setAllBooks(newBooks);
+      setPage(nextPage);
+    }
   };
 
   if (error) return <p>Error :(</p>;
@@ -151,36 +165,35 @@ const Books = () => {
             }}
           />
           {searchTerm && (
-          <Box
-          ref={searchRef}
-          sx={{
-            maxHeight: '300px',
-            width: { xs: '100%', sm: '320px' },
-            overflowY: 'auto',
-            zIndex: 1,
-            border: '1px solid #ccc',
-            borderRadius: '4px',
-            textAlign: 'left',
-            margin: '0 auto',
-            position: 'absolute',
-            top: '100%',
-            left: 0,
-            backgroundColor: '#CFFAFA',
-            '&::-webkit-scrollbar': {
-              width: '6px',
-            },
-            '&::-webkit-scrollbar-track': {
-              background: '#f1f1f1',
-            },
-            '&::-webkit-scrollbar-thumb': {
-              background: '#888',
-              
-            },
-            '&::-webkit-scrollbar-thumb:hover': {
-              background: '#555',
-            },
-          }}
-        >
+            <Box
+              ref={searchRef}
+              sx={{
+                maxHeight: '300px',
+                width: { xs: '100%', sm: '320px' },
+                overflowY: 'auto',
+                zIndex: 1,
+                border: '1px solid #ccc',
+                borderRadius: '4px',
+                textAlign: 'left',
+                margin: '0 auto',
+                position: 'absolute',
+                top: '100%',
+                left: 0,
+                backgroundColor: '#CFFAFA',
+                '&::-webkit-scrollbar': {
+                  width: '6px',
+                },
+                '&::-webkit-scrollbar-track': {
+                  background: '#f1f1f1',
+                },
+                '&::-webkit-scrollbar-thumb': {
+                  background: '#888',
+                },
+                '&::-webkit-scrollbar-thumb:hover': {
+                  background: '#555',
+                },
+              }}
+            >
               {loading ? (
                 Array.from(new Array(3)).map((_, index) => (
                   <Box key={index} sx={{ marginBottom: '10px', display: 'flex', alignItems: 'center', padding: '10px' }}>
@@ -197,52 +210,86 @@ const Books = () => {
               ) : (
                 filteredBooks.map((book, index) => (
                   <Box key={index} sx={{ marginBottom: '10px', display: 'flex', alignItems: 'center', padding: '10px' }}>
-                  <img src={book.coverPhotoURL} alt={book.title} width={80} height={80} style={{ marginRight: '10px' }} />
-                  <div>
-                    <Typography variant="subtitle1" sx={{ fontSize: '0.9rem' }}>{book.title}</Typography>
-                    <Typography variant="caption">Author: {book.author}</Typography>
-                  </div>
-                  <IconButton size="small" onClick={() => handleAddToReadingList(book)} disabled={isBookInReadingList(book)} sx={{ marginLeft: 'auto' }}>
-                    {isBookInReadingList(book) ? (
-                      <CheckCircleOutline sx={{ color: '#335C6E' }} />
-                    ) : (
-                      <AddCircleOutline sx={{ color: '#FABD33' }} />
-                    )}
-                  </IconButton>
-                </Box>
+                    <img src={book.coverPhotoURL} alt={book.title} width={80} height={80} style={{ marginRight: '10px' }} />
+                    <div>
+                      <Typography variant="subtitle1" sx={{ fontSize: '0.9rem' }}>{book.title}</Typography>
+                      <Typography variant="caption">Author: {book.author}</Typography>
+                    </div>
+                    <IconButton size="small" onClick={() => handleAddToReadingList(book)} disabled={isBookInReadingList(book)} sx={{ marginLeft: 'auto' }}>
+                      {isBookInReadingList(book) ? (
+                        <CheckCircleOutline sx={{ color: '#335C6E' }} />
+                      ) : (
+                        <AddCircleOutline sx={{ color: '#FABD33' }} />
+                      )}
+                    </IconButton>
+                  </Box>
                 ))
+              )}
+               {hasError && (
+                <Typography variant="body1" sx={{ padding: '10px', color: '#FF0000' }}>
+                  No books found for "{searchTerm}"
+                </Typography>
               )}
             </Box>
           )}
         </Box>
-        <Grid container spacing={1} justifyContent="center">
-          {loading ? (
-            Array.from(new Array(9)).map((_, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <SkeletonCard />
-              </Grid>
-            ))
-          ) : (
-            randomBooks.map((book, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <BookCard book={book} handleAddToReadingList={handleAddToReadingList} isBookInReadingList={isBookInReadingList(book)} />
-              </Grid>
-            ))
-          )}
-        </Grid>
+        <TeacherFavorites teacherFavorites={teacherFavorites}  loading={loading} handleAddToReadingList={handleAddToReadingList} isBookInReadingList={isBookInReadingList} />
+
+
+        <Box sx={{ marginTop: '40px',  }}>
+        <Typography variant="h5" sx={{ color: '#335E6C' }} gutterBottom textAlign="left">All Books</Typography>
+        
+<Grid container spacing={3} justifyContent="center">
+  {loading ? (
+    Array.from(new Array(6)).map((_, index) => (
+      <Grid item xs={12} sm={6} md={3} key={index} style={{ display: 'flex' }}>
+        <SkeletonCard />
+      </Grid>
+    ))
+  ) : (
+    allBooks.map((book, index) => (
+      <Grid item xs={12} sm={6} md= {3} key={index} style={{ display: 'flex' }}>
+        <BookCard
+          book={book}
+          handleAddToReadingList={handleAddToReadingList}
+          isBookInReadingList={isBookInReadingList(book)}
+        />
+      </Grid>
+    ))
+  )}
+</Grid>
+
+{!loading && data?.books && allBooks.length < data.books.length && (
+  <Button
+    variant="contained"
+    onClick={loadMoreBooks}
+    sx={{
+      marginTop: '20px', // Adjust top margin as needed
+      marginBottom: '20px', // Add bottom margin
+      backgroundColor: '#FADB33',
+      color: '#335C6E',
+      '&:hover': {
+        backgroundColor: '#FAAD00'
+      }
+    }}
+  >
+    Load More
+  </Button>
+)}
+
+
+        </Box>
       </Container>
       <Snackbar
         open={snackbarOpen}
         autoHideDuration={3000}
         onClose={handleSnackbarClose}
-        message="Book added to reading list"
+        message="Added to Reading List"
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        sx={{ bottom: '70px' }}
       />
     </>
   );
 };
-
-
-
-
 
 export default Books;
---
+++ frontend/ello-frontend/src/components/TeacherFavorites.tsx
@@ -0,0 +1,38 @@
+
+import { Grid } from '@mui/material';
+import { Typography}from '@mui/material'
+import SkeletonCard from './SkeletonCard';
+import BookCard from './BookCard';
+import { Book } from './Book';
+
+interface TeacherFavoritesProps {
+  teacherFavorites: Book[];
+  loading: boolean;
+  handleAddToReadingList: (book: Book) => void;
+  isBookInReadingList: (book: Book) => boolean;
+}
+
+const TeacherFavorites = ({ teacherFavorites, loading, handleAddToReadingList, isBookInReadingList }: TeacherFavoritesProps) => {
+  return (
+    <>
+    <Typography variant="h5" sx={{ color: '#335E6C' }} gutterBottom textAlign="left">Teacher Favorites</Typography>
+    <Grid container spacing={3} justifyContent="center" sx={{ marginBottom: '20px' }}>
+      {loading ? (
+        Array.from(new Array(4)).map((_, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <SkeletonCard />
+          </Grid>
+        ))
+      ) : (
+        teacherFavorites.map((book, index) => (
+          <Grid item xs={12} sm={6} md={3} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+            <BookCard book={book} handleAddToReadingList={handleAddToReadingList} isBookInReadingList={isBookInReadingList(book)} />
+          </Grid>
+        ))
+      )}
+    </Grid>
+    </>
+  );
+};
+
+export default TeacherFavorites;
